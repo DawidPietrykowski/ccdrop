141657f78002fde9d57674546f9f5a0413468652
Dawid Pietrykowski <servces+github@dawidpietrykowski.com>
2025-08-01 18:58:05 +0200

    Streming file write, safety, refactor

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,37 +1,79 @@
-use std::io::Write;
+use std::{fmt::Display, pin::pin};
 
 use axum::{
     Router,
-    body::{Body, to_bytes},
+    body::Body,
     extract,
     http::{HeaderMap, StatusCode, header},
     response::IntoResponse,
     routing::{get, post},
 };
+use futures_util::TryStreamExt;
 use rand::{distr::Alphanumeric, *};
 use tokio::{
     fs::File,
-    io::{self},
+    io::{self, BufWriter},
 };
-use tokio_util::io::ReaderStream;
+use tokio_util::io::{ReaderStream, StreamReader};
 
+const ID_LENGTH: usize = 6;
 const SHARE_DIR: &str = "shares";
 
-fn get_share_path(id: &String) -> String {
-    format!("{SHARE_DIR}/{id}")
+enum ShareCreationError {
+    InvalidId,
 }
 
-async fn get_share_object(id: &String) -> io::Result<File> {
+struct ShareId(String);
+
+impl ShareId {
+    fn new_random() -> Self {
+        let new_id: String = rand::rng()
+            .sample_iter(&Alphanumeric)
+            .take(6)
+            .map(char::from)
+            .collect();
+        ShareId(new_id)
+    }
+
+    fn decode(id: String) -> Result<ShareId, ShareCreationError> {
+        if id.len() != ID_LENGTH {
+            return Err(ShareCreationError::InvalidId);
+        }
+        if !id.chars().all(|c| c.is_ascii_alphabetic()) {
+            return Err(ShareCreationError::InvalidId);
+        }
+        Ok(ShareId(id.to_string()))
+    }
+}
+
+impl Display for ShareId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&self.0)
+    }
+}
+
+fn get_share_path(id: &ShareId) -> String {
+    format!("{SHARE_DIR}/{}", id.0)
+}
+
+async fn get_share_object(id: &ShareId) -> io::Result<File> {
     File::open(get_share_path(id)).await
 }
 
-async fn create_share_object(id: &String) -> io::Result<File> {
+async fn create_share_object(id: &ShareId) -> io::Result<File> {
     File::create_new(get_share_path(id)).await
 }
 
 async fn serve_share(extract::Path(id): extract::Path<String>) -> impl IntoResponse {
-    match get_share_object(&id).await {
+    println!("Received request for share id: {id}");
+    let Ok(share_id) = ShareId::decode(id) else {
+        println!("Invalid id");
+        return (StatusCode::UNAUTHORIZED, HeaderMap::new(), Body::empty());
+    };
+
+    match get_share_object(&share_id).await {
         Ok(file) => {
+            println!("Serving share: {share_id}");
             let mut headers = HeaderMap::new();
 
             if let Ok(meta) = file.metadata().await {
@@ -46,29 +88,28 @@ async fn serve_share(extract::Path(id): extract::Path<String>) -> impl IntoRespo
 
             (StatusCode::OK, headers, body)
         }
-        Err(_) => (
-            StatusCode::NOT_FOUND,
-            HeaderMap::new(),
-            axum::body::Body::from("Share not found"),
-        ),
+        Err(_) => {
+            println!("Share not found: {share_id}");
+            (
+                StatusCode::NOT_FOUND,
+                HeaderMap::new(),
+                axum::body::Body::from("Share not found"),
+            )
+        }
     }
 }
 
 async fn generate_share(body: Body) -> impl IntoResponse {
-    let new_id: String = rand::rng()
-        .sample_iter(&Alphanumeric)
-        .take(6)
-        .map(char::from)
-        .collect();
-
-    println!("Generated new id: {new_id}");
-
-    match create_share_object(&new_id).await {
+    let share_id = ShareId::new_random();
+    println!("Generated new id: {share_id}");
+    match create_share_object(&share_id).await {
         Ok(file) => {
-            let bytes = to_bytes(body, usize::MAX).await.unwrap();
-            file.into_std().await.write(&bytes).unwrap();
-            let headers = HeaderMap::new();
-            (StatusCode::OK, headers, Body::from(new_id))
+            let mut file = BufWriter::new(file);
+            let mut stream = StreamReader::new(body.into_data_stream().map_err(io::Error::other));
+            let mut body_reader = pin!(stream);
+            io::copy(&mut body_reader, &mut file).await.unwrap();
+
+            (StatusCode::OK, HeaderMap::new(), Body::from(share_id.0))
         }
         Err(_) => (
             StatusCode::NOT_FOUND,
@@ -84,9 +125,7 @@ async fn main() {
         .route("/get/{id}", get(serve_share))
         .route("/share", post(generate_share));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3331")
-        .await
-        .unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3331").await.unwrap();
 
     axum::serve(listener, app).await.unwrap();
 }
